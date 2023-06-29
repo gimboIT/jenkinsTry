53761b6d56b99dc52acdc00c9cc6e7f67a8423d8
gimbo <gimboy09@mail.ru>
2023-06-30 02:23:34 +0500

    add selenoid

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
@@ -7,7 +8,10 @@ import static com.codeborne.selenide.Selenide.open;
 public class GoogleTest {
     @Test
     void selenideFindInGoogle() {
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
         open("https://www.google.com/");
+        $("#L2AGLb").click();
         $("textarea").setValue("kek").submit();
         $(".hlcw0c").shouldHave(Condition.text("kek"));
     }
